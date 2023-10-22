0517b2af87ee9e231c1e513d8da30c3f1dd88008
seokmin <msm0748@gmail.com>
2023-10-23 03:31:51 +0900

    feat: Set up client socket.io

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,27 @@
+import io from 'socket.io-client';
+import { useEffect } from 'react';
+
+const socket = io('http://localhost:4000');
+
 function App() {
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('connected');
+    });
+
+    return () => {
+      socket.off('connect');
+    };
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit('send_message', { message: 'Hello' });
+  };
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button onClick={sendMessage}>연결</button>
     </div>
   );
 }
