7f81c02db37c889992817bd1389572bcc0a0e0e3
seokmin <msm0748@gmail.com>
2023-10-24 21:37:13 +0900

    feat: Set up react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,12 @@
-import io from 'socket.io-client';
-import { useEffect } from 'react';
-
-const socket = io('http://localhost:4000');
+import { Route, Routes } from 'react-router-dom';
+import ChatList from './pages/chat/ChatList';
 
 function App() {
-  useEffect(() => {
-    socket.on('connect', () => {
-      console.log('connected');
-    });
-
-    return () => {
-      socket.off('connect');
-    };
-  }, []);
-
-  const sendMessage = () => {
-    socket.emit('send_message', { message: 'Hello' });
-  };
-
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button onClick={sendMessage}>연결</button>
-    </div>
+    <Routes>
+      <Route path="/" element={<ChatList />} />
+      {/* <Route path="/chat-item" element={<ChatItem />} /> */}
+    </Routes>
   );
 }
 
